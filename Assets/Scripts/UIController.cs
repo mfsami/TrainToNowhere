3057d2d9732689a362a619b30ef4fc3f34744cf1
Sami El-sayed <samielsayed39@gmail.com>
2025-08-14 15:56:02 -0600

    Clean up / Current car tracking

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,17 +1,31 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    // ------- Buttons -------
     public Button backButton;
+
+    // ------- Text -------
+    public TextMeshProUGUI currentCar;
+
+    // ------- References -------
     public GameManager gameManager;
+
     void Start()
     {
         backButton.onClick.AddListener(OnBackClicked);
     }
 
+    private void Update()
+    {
+        currentCar.text = $"Current Car: {gameManager.currentCarIndex}";
+    }
+
     void OnBackClicked()
     {
         gameManager.MoveBack();
+        
     }
 }
