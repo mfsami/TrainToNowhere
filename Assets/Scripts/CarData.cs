81371cc68ded48ff76f1eae9185d73ae06d4a147
Sami El-sayed <samielsayed39@gmail.com>
2025-09-02 01:52:27 -0600

    Reset game. Added camera look fps

--- Assets/Scripts/CarData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class CarData : MonoBehaviour
-{
-    public Transform cameraTarget;
-
-}
---
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform playerBody;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+    }
+}
